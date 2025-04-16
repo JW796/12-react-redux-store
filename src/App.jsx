a778feb2f9440451cc1373ab071a1ad83f01eb8d
James Joseph <98562132+JW796@users.noreply.github.com>
2025-04-16 14:09:53 -0400

    WIP 3 setup redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-center text-3xl font-medium mt-2 py-4">12 - Redux Store</h1>
     </>
   )
