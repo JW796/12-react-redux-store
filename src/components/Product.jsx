5e40181edba4fb669e8712cffa43555b31b4aa35
James Joseph <98562132+JW796@users.noreply.github.com>
2025-04-19 13:56:07 -0400

    Product style update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,13 @@
-const Product = ({ name, price, image, amount }) => {
+const Product = ({ name, price, image, amount, total }) => {
   return (
-    <div>
-        <img src={image} alt={name + "glasses"} />
-        <div>
-            <p>{name}</p>
-            <p>{price.toFixed(2)}</p>
+    <div className="flex flex-row items-center gap-8 px-10">
+        <img src={image} alt={name + "glasses"} className="max-w-150 min-w-40" />
+        <div className="w-1/2 px-10">
+            <p className="text-xl font-medium">{name}</p>
+            <p className="text-lg">{price.toFixed(2)}</p>
         </div>
-        <div>
-            <p>Amount</p>
+        <div className="flex flex-col items-center">
+            <p className="text-lg font-medium">Amount</p>
             <p>{amount}</p>
         </div>
     </div>
