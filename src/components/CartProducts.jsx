c37e306583eab4e80a4933c4b8d1d4767a9d432c
James Joseph <98562132+JW796@users.noreply.github.com>
2025-04-21 08:17:57 -0400

    Store totals

--- src/components/CartProducts.jsx
+++ src/components/CartProducts.jsx
@@ -5,15 +5,22 @@ const CartProducts = () => {
   const { products, total, amount } = useSelector((store) => store.basket);
   return (
     <div>
-      {products.map((item, i) => (
-        <Product
-          key={new Date().getTime + Math.random()}
-          name={item.name}
-          price={item.price}
-          image={item.image}
-          amount={item.amount}
-        />
-      ))}
+      <div>
+        {products.map((item, i) => (
+          <Product
+            key={new Date().getTime + Math.random()}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+            amount={item.amount}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row items-center justify-evenly py-8">
+        <p>Total</p>
+        <p className="text-2xl font-medium">Total</p>
+        <p className="text-2x font-medium">${total}</p>
+      </div>
     </div>
   );
 };
