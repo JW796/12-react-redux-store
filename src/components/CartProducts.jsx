7424907dee1780d3f4029942c95a3d31484eedb4
James Joseph <98562132+JW796@users.noreply.github.com>
2025-04-26 12:00:23 -0400

    Empty cart handling

--- src/components/CartProducts.jsx
+++ src/components/CartProducts.jsx
@@ -2,24 +2,34 @@ import { useSelector } from "react-redux";
 import Product from "./Product";
 
 const CartProducts = () => {
-  const { products, total } = useSelector((store) => store.cart);
+  const { products, total, amount } = useSelector((store) => store.cart);
   return (
     <div className="py-4">
-      <div>
-        {products.map((item, i) => (
-          <Product
-            key={new Date().getTime + Math.random()}
-            name={item.name}
-            price={item.price}
-            image={item.image}
-            amount={item.amount}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row items-center justify-evenly py-8">
-        <p className="text-2xl font-medium">Total</p>
-        <p className="text-2x font-medium">${total.toFixed(2)}</p>
-      </div>
+      {amount > 1 ? (
+        <>
+          <div>
+            {products.map((item, i) => (
+              <Product
+                key={new Date().getTime + Math.random()}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+                amount={item.amount}
+              />
+            ))}
+          </div>
+          <div className="flex flex-row items-center justify-evenly py-8">
+            <p className="text-2xl font-medium">Total</p>
+            <p className="text-2x font-medium">${total.toFixed(2)}</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="text-2xl text-gray-700 font-medium text-center">
+            There are no Items in your cart!🥲
+          </p>
+        </>
+      )}
     </div>
   );
 };
